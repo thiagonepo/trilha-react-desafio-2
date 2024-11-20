708cf2d347ed9bf0f140a4123b9cadbd8df9e677
Thiago Nepomuceno <90644531+thiagonepo@users.noreply.github.com>
2024-11-19 22:44:16 -0300

    Update App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -34,9 +34,8 @@ function App() {
   }
 
   const handleRemoveRepo = (id) => {
-    console.log('Removendo registro', id);
-
-    // utilizar filter.
+    const updatedRepos = repos.filter(repo => repo.id !== id)
+    setRepos(updatedRepos)
   }
 
 
